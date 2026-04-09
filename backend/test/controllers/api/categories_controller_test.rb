8db0e3a5049f3ca3122e619d04a367d26a8e9c26
TOKIMASA1206 <tokihide121062@gmail.com>
2026-04-09 14:48:17 +0900

    test: add create API tests for categories and spots

---
+++ backend/test/controllers/api/categories_controller_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+class Api::CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "creates a category" do
+    assert_difference("Category.count", 1) do
+      post "/api/categories",
+        params: { category: { name: "Ramen" } },
+        as: :json
+    end
+
+    assert_response :created
+
+    response_json = JSON.parse(response.body)
+
+    assert_equal "Ramen", response_json["name"]
+  end
+
+  test "returns errors when category is invalid" do
+    assert_no_difference("Category.count") do
+      post "/api/categories",
+        params: { category: { name: "" } },
+        as: :json
+    end
+
+    assert_response :unprocessable_entity
+
+    response_json = JSON.parse(response.body)
+
+    assert_includes response_json["errors"], "Name can't be blank"
+  end
+
+  test "returns errors when category name is duplicated" do
+    assert_no_difference("Category.count") do
+      post "/api/categories",
+        params: { category: { name: categories(:one).name } },
+        as: :json
+    end
+
+    assert_response :unprocessable_entity
+
+    response_json = JSON.parse(response.body)
+
+    assert_includes response_json["errors"], "Name has already been taken"
+  end
+end
