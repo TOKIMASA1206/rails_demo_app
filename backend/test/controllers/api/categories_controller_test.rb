dc3a23d023959b100816abce87a5c2f2d00dc62a
TOKIMASA1206 <tokihide121062@gmail.com>
2026-04-13 11:18:39 +0900

    test: add index API test for categories

--- backend/test/controllers/api/categories_controller_test.rb
+++ backend/test/controllers/api/categories_controller_test.rb
@@ -1,6 +1,17 @@
 require "test_helper"
 
 class Api::CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "lists categories" do
+    get "/api/categories"
+
+    assert_response :ok
+
+    response_json = JSON.parse(response.body)
+
+    assert_equal 2, response_json.length
+    assert_equal [categories(:one).id, categories(:two).id].sort, response_json.map { |category| category["id"] }.sort
+  end
+
   test "creates a category" do
     assert_difference("Category.count", 1) do
       post "/api/categories",
